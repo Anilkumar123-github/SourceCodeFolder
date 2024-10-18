1692ac0e9c09f90e7165415eeb21f7474c76a26e
Anilkumar123-github <beeslittle08@gmail.com>
2024-10-19 03:40:12 +0530

    First Commit

---
+++ Super.java
@@ -0,0 +1,28 @@
+//super keyword
+class A
+{
+int p=20;
+void show()
+{
+System.out.println(p);
+}
+}
+class B extends A
+{
+//from class A p=20,show()
+int q=50;
+int p=90;
+void show()
+{
+System.out.println(super.p);
+System.out.println(p);
+}
+}
+class Super
+{
+public static void main(String x[])
+{
+B o = new B();
+o.show();//it gets value from subclass
+}
+}
